d971677f1cc24c61a7aab13eedafec9532b6b7a8
DiegoValdivieso22 <159164145+DiegoValdivieso22@users.noreply.github.com>
2024-04-05 14:07:36 +0000

    edit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+function doMathClicked() {
+  // this function does basic math: Area of a Trapezoid
+  let SideA = parseInt(document.getElementById("SideA").value);
+  let SideB = parseInt(document.getElementById("SideB").value);
+  let SideH = parseInt(document.getElementById("SideH").value); // Update from 'SideC' to 'SideH'
+  let answer = (1/6) * SideA * SideB * SideH;
+
+  // Make sure not negative
+  if (answer <= 0) {
+    answer = 0;
+    document.getElementById("AreaOfTrapezoid").innerHTML = "Invalid input. Ensure value is a positive number.";
+  } else {
+    document.getElementById("AreaOfTrapezoid").innerHTML = "Area of a Trapezoid = " + answer.toFixed(2);
+  }
+}
